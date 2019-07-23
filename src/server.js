ce61fbc07ae0146fac553f78ffa6de7179099cb5
Antimaterium <vitorafonso33@gmail.com>
2019-07-23 00:53:36 -0300

    Add .env basic config - add .env.example; - add dotenv-safe dependencie to load .env;

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+import 'dotenv-safe/config';
 import app from './app';
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.SERVER_PORT, () =>
+  console.log(`Server is listen on ${process.env.SERVER_PORT}`)
+);
